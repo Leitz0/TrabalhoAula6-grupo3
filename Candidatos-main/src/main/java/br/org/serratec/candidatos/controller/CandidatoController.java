742ae25bab05e5d3bab75954a4615d727f6406cb
Leitz0 <162743011+Leitz0@users.noreply.github.com>
2024-05-14 17:12:18 -0300

    Add files via upload

---
+++ Candidatos-main/src/main/java/br/org/serratec/candidatos/controller/CandidatoController.java
@@ -0,0 +1,67 @@
+package br.org.serratec.candidatos.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.org.serratec.candidatos.dto.CandidatoDto;
+import br.org.serratec.candidatos.service.CandidatoService;
+import jakarta.validation.Valid;
+@RestController
+@RequestMapping("/candidatos")
+public class CandidatoController {
+	
+	@Autowired
+	private CandidatoService service;
+	
+	@GetMapping
+	public ResponseEntity <List<CandidatoDto>> buscarTodos() {
+		return ResponseEntity.ok(service.obterTodos());
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public CandidatoDto cadastrarCandidato(@Valid @RequestBody CandidatoDto candidato){
+		return service.incluirCandidato(candidato);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<CandidatoDto> localizarPorId(@PathVariable Long id){
+		Optional<CandidatoDto> candidato = service.obterPorId(id);
+		
+		if(candidato.isPresent()) {
+			return ResponseEntity.ok(candidato.get());
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<CandidatoDto> alterarCandidato(@PathVariable Long id, @RequestBody CandidatoDto candidatoAlt){
+		Optional <CandidatoDto> candidato = service.atualizarCandidato(id, candidatoAlt);
+		
+		if(candidato.isPresent()) {
+			return ResponseEntity.ok(candidato.get());
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> remover(@PathVariable Long id) {
+		if(!service.excluirCandidato(id)){
+		return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.noContent().build();
+	}
+}
---
+++ Candidatos-main/src/main/java/br/org/serratec/candidatos/service/CandidatoService.java
@@ -0,0 +1,57 @@
+package br.org.serratec.candidatos.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.org.serratec.candidatos.dto.CandidatoDto;
+import br.org.serratec.candidatos.model.Candidato;
+import br.org.serratec.candidatos.repository.CandidatoRepositorio;
+
+@Service
+public class CandidatoService {
+
+	@Autowired
+	private CandidatoRepositorio repository;
+	
+	public List<CandidatoDto> obterTodos() {
+		return repository.findAll().stream().map(c-> new CandidatoDto(c.getId(), c.getNome(), c.getNumeroCandidato(),
+				c.getPartido(), c.getCidade(), c.getEstado(), c.getCargo())).toList();
+	}
+	
+	public Optional<CandidatoDto> obterPorId(Long id) {
+		Optional<Candidato> candidato = repository.findById(id);
+		if (candidato.isPresent()) {
+			return Optional.of(candidato.get().toDto());
+		}
+		return Optional.empty();
+	}
+	
+	public CandidatoDto incluirCandidato(CandidatoDto candidato){
+		 Candidato candidatoEntity = repository.save(candidato.toEntity());
+	        return candidatoEntity.toDto();
+		
+	}
+	
+	public Optional<CandidatoDto> atualizarCandidato(Long id, CandidatoDto candidato) {
+		Candidato candidatoEntity = candidato.toEntity();
+		
+		if(repository.existsById(id)) {
+			candidatoEntity.setId(id);
+			repository.save(candidatoEntity);
+			return Optional.of(candidatoEntity.toDto());
+		}
+		return Optional.empty();
+	}
+	
+	public boolean excluirCandidato(Long id) {
+		if(!repository.existsById(id)) {
+			return false;
+		}
+		
+		repository.deleteById(id);
+		return true;
+	}
+}
